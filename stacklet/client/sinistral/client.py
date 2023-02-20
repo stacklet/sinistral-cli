36bd76b1f862ad962820da5fcbdb57d54aee2e9a
thisisshi <theli@outlook.com>
2023-02-19 18:34:26 -0800

    handle query aprams

--- stacklet/client/sinistral/client.py
+++ stacklet/client/sinistral/client.py
@@ -50,21 +50,42 @@ class ClientCommand:
         click.echo(res)
 
     @classmethod
-    def run(cls, **kwargs):
-        ctx = StackletContext(raw_config={})
-        client = SinistralClient(ctx)
+    def handle_query_params(cls, kwargs):
+        lower_map = {}
+        q_params = {}
+        # click passes the kwargs as lowercased version, without the '--'
+        for k in cls.query_params.keys():
+            lower_map = {k[2:].lower(): k[2:]}
+        for k, v in kwargs.items():
+            if k in lower_map:
+                q_params[lower_map[k]] = v
+        return q_params
+
+    @classmethod
+    def handle_json_payload(cls, kwargs):
         payload = {}
         if cls.payload_params:
             json_keys = parse_jsonschema(cls.payload_params["schema"])
             for k, v in kwargs.items():
                 if k in json_keys:
                     payload[k] = v
+        return payload
+
+    @classmethod
+    def run(cls, **kwargs):
+        ctx = StackletContext(raw_config={})
+        client = SinistralClient(ctx)
+
+        payload = cls.handle_json_payload(kwargs)
+        q_params = cls.handle_query_params(kwargs)
+
         res = client.make_request(
-            cls.method,
-            cls.path.format(**kwargs),
+            method=cls.method,
+            path=cls.path.format(**kwargs),
             _json=payload,
             schema=cls.payload_params.get("schema", {}),
             output=kwargs.get("output", "raw"),
+            q_params=q_params,
         )
         return res
 
@@ -79,7 +100,7 @@ class SinistralClient:
             return client()
         raise Exception(f"{name} client not found")
 
-    def make_request(self, method, path, _json={}, output="raw", schema=None):
+    def make_request(self, method, path, _json={}, output="raw", schema=None, q_params={}):
         with StackletContext(self.ctx.config, self.ctx.config.to_json()) as context:
             token = get_token()
             executor = RestExecutor(context, token)
@@ -90,7 +111,7 @@ class SinistralClient:
                 from jsonschema import validate
 
                 validate(_json, schema)
-            res = func(path, _json).json()
+            res = func(path, q_params, _json).json()
             if isinstance(res, dict) and res.get("message") == "Unauthorized":
                 raise Exception("Unauthorized, check credentials")
             fmt = Formatter.registry.get(output, "yaml")()
--- stacklet/client/sinistral/executor.py
+++ stacklet/client/sinistral/executor.py
@@ -16,19 +16,19 @@ class RestExecutor:
         self.session = requests.Session()
         self.session.headers.update({"Authorization": f"Bearer {self.token}"})
 
-    def get(self, path, json=None):
-        return self.session.get(self.api + path)
+    def get(self, path, params={}, json=None):
+        return self.session.get(self.api + path, params=params)
 
-    def post(self, path, json=None):
-        return self.session.post(self.api + path, json=json)
+    def post(self, path, params={}, json=None):
+        return self.session.post(self.api + path, json=json, params=params)
 
 
-def make_request(ctx, method, path, json={}, raw=True):
+def make_request(ctx, method, path, json={}, raw=True, q_params={}):
     with StackletContext(ctx.obj["config"], ctx.obj["raw_config"]) as context:
         token = get_token()
         executor = RestExecutor(context, token)
         func = getattr(executor, method)
-        res = func(path, json).json()
+        res = func(path, q_params, json).json()
         if raw:
             return res
         fmt = Formatter.registry.get(ctx.obj["output"])()
